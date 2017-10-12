d11b2d991e05bc201cabc1db3dc6c4dc999af5f0
Olivier Le Hénaff <lehenaff.olivier@hotmail.fr>
2017-10-12 22:41:18 +0200

    modif dao / class marchandise

---
+++ src/executable/DaoMarchandise.java
@@ -0,0 +1,23 @@
+package executable;
+
+import java.util.ArrayList;
+
+public class DaoMarchandise {
+
+	ArrayList<Crepe> show(){
+		
+	}
+
+	public boolean add(){
+		
+	}
+
+	public boolean update(){
+		
+	}
+
+	public boolean delete(){
+		
+	}
+
+}
---
+++ src/executable/Marchandise.java
@@ -0,0 +1,14 @@
+package executable;
+
+public abstract class Marchandise {
+	
+	protected String name;
+	protected String code;
+	protected double price;
+	
+	
+	public Marchandise(String name, String code, double price){
+		
+	}
+
+}
